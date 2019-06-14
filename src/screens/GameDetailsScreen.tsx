5e0385e0ba7dbd759ef2ed7226fb257c3aa91bea
Marton Borlay <borlaym@gmail.com>
2019-06-14 22:46:25 +0200

    GameDetailsScreen

--- src/screens/GameDetailsScreen.tsx
+++ src/screens/GameDetailsScreen.tsx
@@ -4,39 +4,35 @@ import { State, Group, Game } from '../types';
 import { connect } from 'react-redux';
 
 interface Props {
-	match: match<{ groupId: string, matchId: string }>,
+	match: match<{ groupId: string, gameId: string }>,
 	group?: Group,
 	game?: Game
 };
 
-function MatchListScreen({ group, match }: Props) {
-	console.log(match)
-	if (!group) {
-		return (<h1>Group not found</h1>);
+function MatchListScreen({ group, game }: Props) {
+	if (!group || !game) {
+		return (<h1>Game not found</h1>);
 	}
 	return (
 		<div>
-			<h1>{group.name}</h1>
-			<ul>
-				{group.games.map(game => (
-					<li key={game.id}>
-						{game.players[0]}{game.winner === 0 && '(w)'} {game.score && game.score[0]} -
+			<h1>{group.name} - Game details</h1>
+			<li key={game.id}>
+				{game.players[0]}{game.winner === 0 && '(w)'} {game.score && game.score[0]} -
 						{game.score && game.score[1]} {game.players[1]}{game.winner === 1 && '(w)'}
-						<Link to={`/group/${group.id}/games/${game.id}`}>Details</Link>
-					</li>
-				))}
-			</ul>
+			</li>
 
 		</div>
 	);
 }
 
 function mapStateToProps(state: State, ownProps: Props): Props {
-	console.log(ownProps)
+	const group = state.groups.find(group => group.id === ownProps.match.params.groupId);
+	const game = group ? group.games.find(game => game.id === ownProps.match.params.gameId) : undefined;
 	return {
 		match: ownProps.match,
-		group: state.groups.find(group => group.id === ownProps.match.params.groupId)
-	}
+		group,
+		game 
+	};
 }
 
 export default connect(mapStateToProps)(MatchListScreen);
