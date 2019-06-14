b9bad37b189a927ea4c338721558505bd06a19fe
Marton Borlay <borlaym@gmail.com>
2019-06-15 00:04:04 +0200

    Use HashRouter to work with gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import SelectGroupScreen from './screens/SelectGroupScreen';
 import GameListScreen from './screens/GameListScreen';
 import GameDetailsScreen from './screens/GameDetailsScreen';
