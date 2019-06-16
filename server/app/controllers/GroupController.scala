283726fc82d261645e3e3444fd7a5a8d8d78d121
Marton Borlay <borlaym@gmail.com>
2019-06-16 22:55:14 +0200

    Get all groups

--- server/app/controllers/GroupController.scala
+++ server/app/controllers/GroupController.scala
@@ -13,13 +13,13 @@ import play.api.libs.json.Json
 @Singleton
 class GroupController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  val groups: Map[String, Group] = Map(
-    "go" -> Group(23312, "Go", List(
+  val groups: Map[Long, Group] = Map(
+    23312.toLong -> Group(23312, "Go", List(
       Game(1, List("Ed", "Viktor"), Some(List(21, 22)), 1),
       Game(2, List("Ed", "Viktor"), Some(List(5, 12)), 1),
       Game(3, List("Ed", "Viktor"), Some(List(25, 1)), 0)
     )),
-    "chess" -> Group(21311, "Chess", List(
+    21311.toLong -> Group(21311, "Chess", List(
       Game(4, List("Marci", "Viktor"), None, 0),
       Game(5, List("Marci", "Viktor"), None, 1),
       Game(6, List("Marci", "Viktor"), None, 0)
@@ -27,11 +27,15 @@ class GroupController @Inject()(cc: ControllerComponents) extends AbstractContro
   )
 
 
-  def getGroupById(groupId: String) = Action { implicit request: Request[AnyContent] =>
+  def getGroupById(groupId: Long) = Action { implicit request: Request[AnyContent] =>
     val group = groups.get(groupId)
     group match {
       case Some(group) => Ok(Json.toJson(group))
       case _ => NotFound("Group not found")
     }
+  }
+
+  def getAllGroups() = Action { implicit request: Request[AnyContent] =>
+      Ok(Json.toJson(groups.map{ case (_, group) => group }))
   }
 }
